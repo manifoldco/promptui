1b359d275bb95a3b498524587e4b0f539edba948
҈҈҈Luiz Branco <me@luizbranco.com>
2017-10-18 12:49:04 -0400

    Make sure generic error doesnt freeze prompt (#7)

---
+++ examples/prompt/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/manifoldco/promptui"
+)
+
+func main() {
+	validate := func(input string) error {
+		_, err := strconv.ParseFloat(input, 64)
+		return err
+	}
+
+	prompt := promptui.Prompt{
+		Label:    "Number",
+		Validate: validate,
+	}
+
+	result, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	fmt.Printf("You choose %q\n", result)
+}
