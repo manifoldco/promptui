6c8026db80a52de981dcd68d84b0791efaed21f0
Luiz Branco <me@luizbranco.com>
2017-10-19 13:08:29 -0400

    Add more examples

---
+++ _examples/confirm/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/manifoldco/promptui"
+)
+
+func main() {
+	prompt := promptui.Prompt{
+		Label:     "Delete Resource",
+		IsConfirm: true,
+	}
+
+	result, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	fmt.Printf("You choose %q\n", result)
+}
