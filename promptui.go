9635a9ccfa0293af4db13805852afa2b571fd8e7
Luiz Branco <me@luizbranco.com>
2017-10-20 07:25:20 -0400

    Remove unused error
    Any validation error will trigger the message now.

--- promptui.go
+++ promptui.go
@@ -17,22 +17,6 @@ var ErrAbort = errors.New("")
 // if the input is not valid.
 type ValidateFunc func(string) error
 
-// ValidationError is the class of errors resulting from invalid inputs,
-// returned from a ValidateFunc.
-type ValidationError struct {
-	msg string
-}
-
-// Error implements the error interface for ValidationError.
-func (v *ValidationError) Error() string {
-	return v.msg
-}
-
-// NewValidationError creates a new validation error with the given message.
-func NewValidationError(msg string) *ValidationError {
-	return &ValidationError{msg: msg}
-}
-
 // SuccessfulValue returns a value as if it were entered via prompt, valid
 func SuccessfulValue(label, value string) string {
 	return IconGood + " " + label + ": " + faint(value)
