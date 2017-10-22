99f8da6560868729ad0d2fcdc0c66eb00d9653d9
Luiz Branco <me@luizbranco.com>
2017-10-22 13:23:16 -0400

    Add documentation

--- list/list.go
+++ list/list.go
@@ -5,14 +5,23 @@ import (
 	"reflect"
 )
 
+// List holds a collection of items that can be displayed with an N number of
+// visible items. The list can be moved up, down by one item of time or an
+// entire page (ie: visible size). It keeps track of the current selected item.
 type List struct {
 	items  []interface{}
-	cursor int
-	size   int
+	cursor int // cursor holds the index of the current selected item
+	size   int // size is the number of visible options
 	start  int
 }
 
+// New creates and initializes a list. Items must be a slice type and size must
+// be greater than 0.
 func New(items interface{}, size int) (*List, error) {
+	if size < 1 {
+		return nil, fmt.Errorf("list size %d must be greater than 0", size)
+	}
+
 	if items == nil || reflect.TypeOf(items).Kind() != reflect.Slice {
 		return nil, fmt.Errorf("items %v is not a slice", items)
 	}
@@ -27,6 +36,9 @@ func New(items interface{}, size int) (*List, error) {
 	return l, nil
 }
 
+// Prev moves the visible list back one item. If the selected item is out of
+// view, the new select item becomes the last visible item. If the list is
+// already at the top, nothing happens.
 func (l *List) Prev() {
 	if l.cursor > 0 {
 		l.cursor--
@@ -37,6 +49,9 @@ func (l *List) Prev() {
 	}
 }
 
+// Next moves the visible list forward one item. If the selected item is out of
+// view, the new select item becomes the first visible item. If the list is
+// already at the bottom, nothing happens.
 func (l *List) Next() {
 	max := len(l.items) - 1
 
@@ -49,6 +64,10 @@ func (l *List) Next() {
 	}
 }
 
+// PageUp moves the visible list backward by x items. Where x is the size of the
+// visible items on the list. The selected item becomes the first visible item.
+// If the list is already at the bottom, the selected item becomes the last
+// visible item.
 func (l *List) PageUp() {
 	start := l.start - l.size
 	if start < 0 {
@@ -64,6 +83,9 @@ func (l *List) PageUp() {
 	}
 }
 
+// PageDown moves the visible list forward by x items. Where x is the size of
+// the visible items on the list. The selected item becomes the first visible
+// item.
 func (l *List) PageDown() {
 	start := l.start + l.size
 	max := len(l.items) - l.size
@@ -84,19 +106,24 @@ func (l *List) PageDown() {
 	}
 }
 
+// CanPageDown returns whether a list can still PageDown().
 func (l *List) CanPageDown() bool {
 	max := len(l.items)
 	return l.start+l.size < max
 }
 
+// CanPageUp returns whether a list can still PageUp().
 func (l *List) CanPageUp() bool {
 	return l.start > 0
 }
 
+// Selected returns the item currently selected.
 func (l *List) Selected() interface{} {
 	return l.items[l.cursor]
 }
 
+// Display returns a slice equal to the size of the list with the current
+// visible items.
 func (l *List) Display() []interface{} {
 	var result []interface{}
 	end := l.start + l.size
